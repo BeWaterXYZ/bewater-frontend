adc1e2ec14f281c612a6e59c043601f6e3e4a9cb
muvich3n <muvich3n@gmail.com>
2022-10-17 22:53:57 +0800

    add a prop for button

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -12,14 +12,34 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = () => {
-  return (
-    <div className="inline-flex gap-x-2">
-      <Button text="Add Role" />
-      <Button text="Save Changes" />
-      <Button text="Disconnect" />
-    </div>
-  );
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Save Changes',
+  type: 'primary',
 };
 
-export const Normal = Template.bind({});
+export const Secondary1 = Template.bind({});
+Secondary1.args = {
+  text: 'Add Role',
+  type: 'secondary',
+};
+
+export const Secondary2 = Template.bind({});
+Secondary2.args = {
+  text: 'Add Skill',
+  type: 'secondary',
+};
+
+export const Danger1 = Template.bind({});
+Danger1.args = {
+  text: 'Disconnect',
+  type: 'danger',
+};
+
+export const Danger2 = Template.bind({});
+Danger2.args = {
+  text: 'Deactivate Account',
+  type: 'danger',
+};
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,20 @@
 import clsx from 'clsx';
 
 interface Props {
-  text?: string;
-  type?: string;
+  text: string;
+  type: string;
   className?: string;
 }
 
-export const Button = ({ text, className }: Props) => {
+export const Button = ({ text, type, className }: Props) => {
   return (
     <div
       className={clsx(
-        'inline-flex typ-body py-1 px-4 border-2 border-solid rounded-[7px] cursor-pointer w-auto hover:brightness-90 transition duration-[.15s] ease-out border-bw-fore bg-bw-back text-bw-fore',
+        'inline-flex typ-body py-1 px-4 border-2 border-solid rounded-[7px] cursor-pointer w-auto hover:brightness-90 transition duration-[.15s] ease-out',
         {
-          'border-bw-fore bg-bw-fore text-bw-back': text === 'Save Changes',
-          'border-[#DD2828] bg-bw-back text-[#DD2828]': text === 'Disconnect',
+          'border-bw-fore bg-bw-fore text-bw-back': type === 'primary',
+          'border-bw-fore bg-bw-back text-bw-fore': type === 'secondary',
+          'border-[#DD2828] bg-bw-back text-[#DD2828]': type === 'danger',
         },
         className,
       )}
