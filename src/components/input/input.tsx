6f3852c217e1d9d44a584bbc301fa81f155c582e
muvich3n <muvich3n@gmail.com>
2022-10-31 15:46:07 +0800

    Revert "transfer input&button style to mui base component"
    This reverts commit a968497b79adaf2fa504636835d744f1e31b9f51.

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,57 +1,31 @@
-import * as React from 'react';
-import InputUnstyled, {
-  InputUnstyledProps,
-  InputUnstyledInputSlotProps,
-} from '@mui/base/InputUnstyled';
-
 import clsx from 'clsx';
-
-const CustomInput = React.forwardRef(function CustomInput(
-  props: InputUnstyledInputSlotProps,
-  ref: React.ForwardedRef<HTMLInputElement>,
-) {
-  return (
-    <input
-      {...props}
-      className={
-        'typ-body py-1 px-4 h-8 box-border border border-solid bg-bw-back rounded-button w-full max-w-[400px] mr-4 transition duration-[.15s] focus:outline-none ease'
-      }
-      ref={ref}
-    />
-  );
-});
 
 interface Props {
   placeholder?: string;
   type: string;
   className?: string;
-  helpText?: string;
 }
 
-export const Input = ({ placeholder, type, helpText, className }: Props) => {
+export const Input = ({ placeholder, type, className }: Props) => {
   return (
     <div className="block items-center">
-      <CustomInput
+      <input
         placeholder={placeholder}
-        type="text"
-        disabled={type === 'disabled' ? true : false}
-        error={type === 'error' ? true : false}
-        multiline={type === 'multiline' ? true : false}
         className={clsx(
-          '',
+          'typ-body py-1 px-4 h-8 box-border border border-solid bg-bw-back rounded-button w-full max-w-[400px] mr-4 transition duration-[.15s] focus:outline-none ease',
           {
-            'border-[#E4E4E4] text-bw-fore text-opacity-30 bg-[#F7F7F7':
+            'border-[#E4E4E4] cursor-not-allowed pointer-events-none text-bw-fore text-opacity-30 bg-[#F7F7F7':
               type === 'disabled',
-            'border-[#E4E4E4] text-bw-fore hover:border-[#d0d0d0] focus:border-[#999999]':
+            'border-[#E4E4E4] text-bw-fore hover:border-[#d0d0d0] focus:border-[#999]':
               type === 'normal',
             'border-[#EB7E7E] placeholder:text-[#DD2828]': type === 'error',
           },
           className,
         )}
-      />
+      ></input>
       {type === 'error' && (
         <a className="inline-block whitespace-nowrap typ-body py-1 text-[#DD2828]">
-          {helpText}
+          This username has been taken.
         </a>
       )}
     </div>
