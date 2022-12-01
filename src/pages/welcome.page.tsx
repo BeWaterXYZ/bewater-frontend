16ab0277ca5e840d942254b4fb70a9582ba7a11f
Nick Jiang <nicolazj@gmail.com>
2022-12-01 22:09:38 +1300

    fix missing props

--- src/pages/welcome.page.tsx
+++ src/pages/welcome.page.tsx
@@ -4,6 +4,7 @@ import { useAlert } from '@/components/alert';
 import { useAuthContext } from '@/hooks/useAuth';
 
 import type { NextPage } from 'next';
+import { useAuthStore } from '@/stores/auth';
 
 // Pass client to React Context Provider
 const PageWelcome: NextPage = () => {
@@ -11,6 +12,7 @@ const PageWelcome: NextPage = () => {
     title: 'An error occurs',
     text: 'Create user failed, please visit the site later',
   });
+  const user = useAuthStore((s) => s.user);
   return (
     <>
       <div className="flex flex-col h-[calc(100vh-160px)] justify-center items-center">
@@ -18,7 +20,7 @@ const PageWelcome: NextPage = () => {
           <div className="bg-linearWelcome bg-clip-text text-transparent typ-h2">
             {'Welcome aboard,'}
           </div>
-          <FormWelcome onError={onAlert} />
+          <FormWelcome user={user} onError={onAlert} />
         </div>
       </div>
       <Alert />
