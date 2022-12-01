1eb672147de79a3eb5244e8e8ee05e1afde5f6d2
Nick Jiang <nicolazj@gmail.com>
2022-12-01 22:27:55 +1300

    update menu

--- src/components/menu/menu-item.tsx
+++ src/components/menu/menu-item.tsx
@@ -4,9 +4,10 @@ import clsx from 'clsx';
 import { useMenuActions } from '@/hooks/useMenuActions';
 
 import type { MenuItemType } from '@/models/menu';
+import Link from 'next/link';
 
 type Props = {
-  item?: MenuItemType;
+  item: MenuItemType;
   label?: string;
   isOpen: boolean;
   hasSubMenu: boolean;
@@ -32,22 +33,23 @@ export const MenuItem = React.forwardRef(function MenuItem(
     : {};
 
   return (
-    <a
-      className={clsx(
-        'relative flex items-center h-full typ-h5 outline-none after:absolute after:left-0 after:right-0 after:bottom-0 after:h-[3px] after:bg-bw-fore after:rounded-full after:opacity-0 after:transition-opacity after:duration-[.15s] after:ease-out hover:after:opacity-100 focus:after:opacity-100',
-        isOpen || isActive ? 'after:opacity-100' : 'after:opacity-0',
-        {
-          'after:opacity-100': isActive,
-          'cursor-pointer': !hasSubMenu,
-        },
-      )}
-      href={item?.path}
-      {...externalProps}
-      onClick={handleActionMemo}
-      ref={ref}
-      tabIndex={0}
-    >
-      {item?.label ? item?.label : label}
-    </a>
+    <Link href={item.path ?? '/'}>
+      <a
+        className={clsx(
+          'relative flex items-center h-full typ-h5 outline-none after:absolute after:left-0 after:right-0 after:bottom-0 after:h-[3px] after:bg-bw-fore after:rounded-full after:opacity-0 after:transition-opacity after:duration-[.15s] after:ease-out hover:after:opacity-100 focus:after:opacity-100',
+          isOpen || isActive ? 'after:opacity-100' : 'after:opacity-0',
+          {
+            'after:opacity-100': isActive,
+            'cursor-pointer': !hasSubMenu,
+          },
+        )}
+        {...externalProps}
+        onClick={handleActionMemo}
+        ref={ref}
+        tabIndex={0}
+      >
+        {item?.label ? item?.label : label}
+      </a>
+    </Link>
   );
 });
--- src/hooks/useMenuData.tsx
+++ src/hooks/useMenuData.tsx
@@ -1,27 +1,23 @@
-import { useEffect, useState } from 'react';
-
-import { urlWithBasePath } from '@/utils/urlWithBasePath';
-
-import type { MenuData, MenuItemType } from '@/models/menu';
+import type { MenuItemType } from '@/models/menu';
 
 const mainMenu: MenuItemType[] = [
   {
     type: 'menu-item',
     key: 'challenges',
     label: 'Challenges',
-    path: urlWithBasePath('/challenges'),
+    path: '/challenges',
   },
   {
     type: 'menu-item',
     key: 'showcase',
     label: 'Showcase',
-    path: urlWithBasePath('/showcase'),
+    path: '/showcase',
   },
   {
     type: 'menu-item',
     key: 'docs',
     label: 'Docs',
-    path: urlWithBasePath('/docs'),
+    path: '/docs',
   },
 ];
 
@@ -30,41 +26,37 @@ const profileMenu: MenuItemType[] = [
     type: 'menu-item',
     key: 'user_profile',
     label: 'Your Profile',
-    path: urlWithBasePath('/user/profile'),
+    path: '/user/profile',
   },
   {
     type: 'menu-item',
     key: 'user_settings',
     label: 'Account Settings',
-    path: urlWithBasePath('/user/settings'),
+    path: '/user/settings',
   },
 ];
 
 export function useMenuData() {
-  const [menuData, setMenuData] = useState<MenuData>();
-
-  useEffect(() => {
-    setMenuData({
-      main: mainMenu.map((menu) => {
-        return {
-          type: menu.type,
-          key: menu.key,
-          label: menu.label,
-          path: menu.path,
-          external: menu.external,
-        };
-      }),
-      profile: profileMenu.map((menu) => {
-        return {
-          type: menu.type,
-          key: menu.key,
-          label: menu.label,
-          path: menu.path,
-          external: menu.external,
-        };
-      }),
-    });
-  }, []);
+  const menuData = {
+    main: mainMenu.map((menu) => {
+      return {
+        type: menu.type,
+        key: menu.key,
+        label: menu.label,
+        path: menu.path,
+        external: menu.external,
+      };
+    }),
+    profile: profileMenu.map((menu) => {
+      return {
+        type: menu.type,
+        key: menu.key,
+        label: menu.label,
+        path: menu.path,
+        external: menu.external,
+      };
+    }),
+  };
 
   return {
     menuData,
