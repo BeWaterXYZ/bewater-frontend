1b2a386015012efdff21598218ef26a15bf6f4f4
Nick Jiang <nicolazj@gmail.com>
2022-12-05 14:15:18 +1300

    add linting

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 
-// Pass client to React Context Provider
 const PageIndex: NextPage = () => {
   return (
     <div className="flex flex-col h-[calc(100vh-160px)] justify-center items-center">
