829772d492d4b057e39edbea9a6f1310c9224d53
Nick Jiang <nicolazj@gmail.com>
2022-12-06 15:32:20 +1300

    update sentry config

--- next.config.js
+++ next.config.js
@@ -92,7 +92,7 @@ const sentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = isInGithubAction
-  ? { nextConfig }
+  ? nextConfig
   : withSentryConfig(
       { ...nextConfig, ...sentryModuleExports },
       sentryWebpackPluginOptions,
