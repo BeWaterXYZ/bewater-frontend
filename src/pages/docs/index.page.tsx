f85f7c0941cf00989a6f7d6b8b84439aaef5cf89
Nicolas Jiang <nicolazj@gmail.com>
2022-12-07 13:59:04 +1300

    update font (#46)

--- src/pages/docs/index.page.tsx
+++ src/pages/docs/index.page.tsx
@@ -5,9 +5,7 @@ import type { NextPage } from 'next';
 // Pass client to React Context Provider
 const PageDocs: NextPage = () => {
   return (
-    <div className="flex flex-col h-[calc(100vh-160px)] justify-center items-center">
-      Docs Page
-    </div>
+    <div className="flex flex-col  justify-center items-center">Docs Page</div>
   );
 };
 
--- src/pages/showcase/index.page.tsx
+++ src/pages/showcase/index.page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from 'next';
 // Pass client to React Context Provider
 const PageShowcase: NextPage = () => {
   return (
-    <div className="flex flex-col h-[calc(100vh-160px)] justify-center items-center">
+    <div className="flex flex-col  justify-center items-center">
       Showcase Page
     </div>
   );
