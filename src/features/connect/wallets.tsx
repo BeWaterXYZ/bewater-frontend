508d2dccba2add52ac26e55c01c9702645f5609f
Nick Jiang <nicolazj@gmail.com>
2022-12-07 16:38:22 +1300

    not to use wagmi hooks

--- src/features/connect/wallets.tsx
+++ src/features/connect/wallets.tsx
@@ -1,115 +1,69 @@
 import clsx from 'clsx';
-import { useEffect } from 'react';
 import { useToggle } from 'react-use';
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useNetwork,
-  useSignMessage,
-} from 'wagmi';
+import { useConnect } from 'wagmi';
 
 import { Loading } from '@/components/loading';
 import { Logo } from '@/components/logos';
 import useNavigator from '@/hooks/useNavigator';
-import {
-  submitGetLoginMessage,
-  submitVerifySignedMessage,
-} from '@/services/auth';
 import { useAuthStore } from '@/stores/auth';
 import { useModalStore } from '@/stores/modal';
+
+import { connectWallet, startSignMsgAndVerify } from './connect';
 
 import type { Connector } from 'wagmi';
 
 interface Props {
   onError?: (text?: string) => void;
 }
-
+function isErrorWithName(error: unknown): error is { name: string } {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as Record<string, unknown>).name === 'string'
+  );
+}
 export function WalletOptions({ onError }: Props) {
-  const isAuthed = useAuthStore((s) => s.isAuthed);
   const setAuthState = useAuthStore((s) => s.setState);
-  const [startLogin, setStartLogin] = useToggle(false);
   const [isLogining, setIsLogining] = useToggle(false);
-  const { isConnected, connector: activeConnector, address } = useAccount();
-  const { disconnect } = useDisconnect();
   const navigator = useNavigator();
-  const { chain } = useNetwork();
-  const { signMessageAsync } = useSignMessage();
   const open = useModalStore((s) => s.open);
-  const { connect, connectors, isLoading } = useConnect({
-    onError(error, variables) {
-      if (error.name === 'ConnectorNotFoundError') {
-        if (variables.connector.id === 'metaMask') {
-          open('metamask');
+  const { connectors } = useConnect();
+
+  const onConnectorClick = async (connector: Connector) => {
+    try {
+      setIsLogining(true);
+      const { address, chainId } = await connectWallet(connector);
+      if (address && chainId) {
+        const { token, userId, userProfile } = await startSignMsgAndVerify(
+          address,
+          chainId,
+        );
+
+        setAuthState({
+          token,
+          user: { userId, walletAddress: address, isNewUser: !userProfile },
+        });
+
+        if (!userProfile) {
+          navigator.goToWelcome();
+        } else {
+          navigator.goToUserSettings();
         }
       }
-    },
-  });
-
-  useEffect(() => {
-    const asyncAuth = async () => {
-      try {
-        setIsLogining(true);
-        if (address && chain?.id) {
-          const messageParams = {
-            walletAddress: address,
-            chain: `${chain?.id}`,
-            network: 'evm',
-          };
-          const { message } = await submitGetLoginMessage(messageParams);
-          const signature = await signMessageAsync({ message });
-          const { token, userId, userProfile } =
-            await submitVerifySignedMessage({
-              message,
-              signature,
-            });
-
-          setAuthState({
-            token,
-            user: { userId, walletAddress: address, isNewUser: !userProfile },
-          });
-
-          if (!userProfile) {
-            navigator.goToWelcome();
-          } else {
-            navigator.goToUserSettings();
+    } catch (error: unknown) {
+      if (isErrorWithName(error)) {
+        if (error?.name === 'ConnectorNotFoundError') {
+          if (connector.id === 'metaMask') {
+            open('metamask');
+            return;
           }
         }
-      } catch (error) {
-        console.error('asyncAuth::', error);
-        onError && onError('Connect Wallet Failed. Please try again.');
-        // TODO: logger
-      } finally {
-        setIsLogining(false);
-        setStartLogin(false);
-        disconnect();
       }
-    };
-    if (startLogin && !isLogining && !isLoading && isConnected && !isAuthed()) {
-      void asyncAuth();
-    }
-  }, [
-    isConnected,
-    activeConnector,
-    navigator,
-    address,
-    chain,
-    startLogin,
-    isLogining,
-    isLoading,
-    setIsLogining,
-    setStartLogin,
-    signMessageAsync,
-    disconnect,
-    onError,
-    isAuthed,
-    setAuthState,
-  ]);
 
-  const onClick = (connector: Connector) => {
-    setStartLogin(true);
-    if (!isConnected || activeConnector?.id !== connector.id) {
-      connect({ connector });
+      onError && onError('Connect Wallet Failed. Please try again.');
+    } finally {
+      setIsLogining(false);
     }
   };
 
@@ -119,16 +73,15 @@ export function WalletOptions({ onError }: Props) {
         {connectors.map((connector, i) => (
           <button
             className={clsx(
-              'w-full h-12 border-t border-l border-r border-solid border-[#E4E4E4] flex flex-row items-center px-4 gap-x-4',
-              'hover:bg-[#E4E4E4]',
+              'w-full h-12 border-t border-l border-r border-solid border-border flex flex-row items-center px-4 gap-x-4',
+              'hover:bg-border',
               {
                 'rounded-t-md': i === 0,
                 'rounded-b-md border-b': i === connectors.length - 1,
               },
             )}
-            // disabled={!connector.ready}
             key={connector.id}
-            onClick={() => onClick(connector)}
+            onClick={() => void onConnectorClick(connector)}
           >
             <Logo code={connector.name} />
             <span>{connector.name}</span>
@@ -136,7 +89,7 @@ export function WalletOptions({ onError }: Props) {
           </button>
         ))}
       </div>
-      {isLoading || isLogining ? <Loading /> : null}
+      {isLogining ? <Loading /> : null}
     </>
   );
 }
