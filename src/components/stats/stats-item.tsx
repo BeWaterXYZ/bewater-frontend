50fb24da75b782965c056dd35667957168a614b8
Nick Jiang <nicolazj@gmail.com>
2022-12-12 22:06:36 +1300

    update

--- src/components/stats/stats-item.tsx
+++ src/components/stats/stats-item.tsx
@@ -11,13 +11,13 @@ export const StatsItem = ({ title, description, type, className }: Props) => {
   return (
     <div
       className={clsx(
-        'flex flex-col gap-2 py-3 px-4 box-border border border-[#E4E4E4] border-solid bg-bw-back rounded-md w-full max-w-[192px] h-20 transition duration-[.15s] ease',
+        'flex flex-col gap-2 py-3 px-4 box-border border border-titanium border-solid bg-bw-back rounded-md w-full max-w-[192px] h-20 transition duration-[.15s] ease',
         className,
       )}
     >
       <div className="body-1">{title}</div>
       {type === 'number' ? (
-        <div className="typ-number">{description}</div>
+        <div className="body-2">{description}</div>
       ) : (
         <div className="body-1">{description}</div>
       )}
