5a6e75333ea2de1383d7c81c2ee3a00355ec24e4
Kuma Li <li.jun.kuma@gmail.com>
2023-01-27 17:06:01 +0800

    fix connect button style on header

--- src/components/header/user/index.tsx
+++ src/components/header/user/index.tsx
@@ -8,7 +8,10 @@ export default function UserArea() {
   const user = useAuthStore((s) => s.user);
 
   return !isAuthed() ? (
-    <Link href="/connect" className="btn btn-primary">
+    <Link
+      href="/connect"
+      className="btn h-12 py-4 px-8 uppercase mono-4 text-day border-day bg-transparent"
+    >
       Connect
     </Link>
   ) : (
