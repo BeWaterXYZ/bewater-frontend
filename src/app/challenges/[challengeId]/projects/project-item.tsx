ce176e4a80038aa0e2f6177e53a3f89ae867673d
Nicolas Jiang <nicolazj@gmail.com>
2023-02-21 12:23:16 +1300

    UI updates

--- src/app/challenges/[challengeId]/projects/project-item.tsx
+++ src/app/challenges/[challengeId]/projects/project-item.tsx
@@ -20,26 +20,30 @@ export function ProjectItem({ project }: { project: Project }) {
             className="object-cover"
           />
         </Aspect>
-        <div className="absolute top-[8px] right-[8px]">
+        <div className="absolute top-[8px] right-[8px] flex gap-2">
           {project.tags.map((tag) => (
-            <TagProjectTag
-              label={tag}
+            <div
               key={tag}
-              className="border-none !bg-[rgba(0,_0,_0,_0.3)] backdrop-blur-[2px]"
-            />
+              className="inline h-6 rounded  gap-1 bg-[rgba(0,_0,_0,_0.3)] backdrop-blur-[2px]"
+            >
+              <TagProjectTag
+                label={tag}
+                className="border-none relative -top-[2px] "
+              />
+            </div>
           ))}
         </div>
       </Link>
-      <div className="p-4 bg-white/5 flex-1">
+      <div className="p-4 bg-white/5 flex-1 flex flex-col">
         <Link
           href={`challenges/${project.team.challengeId}/projects/${project.id}`}
         >
           <h2 className="heading-6 mb-2">{project.name}</h2>
-          <p className="body-3 text-gray-400 text-left">
+          <p className="body-3 text-gray-400 text-left ">
             {project.description}
           </p>
         </Link>
-        <div className="h-[20px] invisible">_</div>
+        <div className="h-[20px] invisible flex-1">_</div>
         <Link
           href={`challenges/${project.team.challengeId}/teams/${project.teamId}`}
           className="body-3 text-left mb-2"
