713409af7725030c431b4c41c774f14768a4b474
muvich3n <muvich3n@gmail.com>
2023-04-24 14:51:57 +0800

    update header height & label text leading

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,13 +21,13 @@ export const HeaderImpl = ({ logo, nav, user }: HeaderImplProps) => {
       className="fixed z-10  top-0 left-0 right-0 text-black w-full flex flex-shrink-0 justify-center items-center transition-colors"
     >
       <div className=" flex items-center justify-between container flex-wrap">
-        <div className="w-1/2 order-1 md:w-1/5 flex justify-start h-16 items-center">
+        <div className="w-1/2 order-1 md:w-1/5 flex justify-start h-[72px] items-center">
           {logo}
         </div>
         <div className="w-full order-3 md:order-2 md:w-3/5 flex justify-center ">
           {nav}
         </div>
-        <div className="w-1/2  order-2 md:order-3 md:w-1/5 flex justify-end h-16 items-center">
+        <div className="w-1/2  order-2 md:order-3 md:w-1/5 flex justify-end h-[72px] items-center">
           {user}
         </div>
       </div>
@@ -45,8 +45,10 @@ const BeWaterLogo = () => {
         height={24}
         alt="bewater logo"
       />
-      <div className="body-5 text-day absolute left-full top-[-12px] rounded-full p-[1px] bg-gradient-to-r from-[#057382]  to-[#66FFFF]">
-        <div className="bg-night rounded-full px-[6px]">Alpha</div>
+      <div className="body-5 text-day absolute left-full top-[-12px] rounded-full p-[1px] bg-gradient-to-r from-[#057382] to-[#66FFFF]">
+        <div className="bg-night rounded-full px-[5px] leading-[12px]">
+          Alpha
+        </div>
       </div>
     </Link>
   );
