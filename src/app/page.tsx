e64de691366bdd8adf8a69bc5422703851e3f9c7
Nicolas Jiang <nicolazj@gmail.com>
2023-04-27 13:12:17 +1200

    if only 1 challenge, goto detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { getChallenges } from '@/services/challenge';
 import { redirect } from 'next/navigation';
 export default async function Home() {
+  let challenges = await getChallenges();
+  let active = challenges.filter((c) => c.status === 'ACTIVE');
+  if (active.length === 1) {
+    return redirect(`/challenges/${active[0].id}`);
+  }
   return redirect('/challenges');
 }
