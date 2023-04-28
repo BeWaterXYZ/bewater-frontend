0a6c7031c95e976c6358f0d2ce5e1fbf1e5c05a7
Nicolas Jiang <nicolazj@gmail.com>
2023-04-28 13:44:48 +1200

    new intro

--- src/app/challenges/[challengeId]/hero.tsx
+++ src/app/challenges/[challengeId]/hero.tsx
@@ -1,10 +1,14 @@
 import { Challenge } from '@/services/types';
 import { formatMMMDDYYYY } from '@/utils/date';
+import { isMileStoneEnabled } from './utils';
+import Link from 'next/link';
 
 interface ChallengeHeroProps {
   challenge: Challenge;
 }
 export function ChallengeHero({ challenge }: ChallengeHeroProps) {
+  const isTeamingEnabled = isMileStoneEnabled('Teaming', challenge);
+
   return (
     <div className=" relative overflow-hidden pb-7 lg:pb-[192px] pt-[93px] lg:pt-[224px] text-center flex flex-col justify-center bg-[url(/challenge/bg.png)] bg-cover ">
       <p className="body-4 lg:text-[20px]">{challenge.hostName}</p>
@@ -15,6 +19,17 @@ export function ChallengeHero({ challenge }: ChallengeHeroProps) {
           challenge.endTime,
         )}`}
       </h1>
+
+      {isTeamingEnabled ? (
+        <div className="mt-10">
+          <Link
+            href={`/challenges/${challenge.id}/teams`}
+            className="btn btn-primary-invert body-4 text-day  uppercase w-64 py-6"
+          >
+            Join / create a team
+          </Link>
+        </div>
+      ) : null}
       <div className="absolute  hidden lg:block lg:w-[100px] lg:h-[1388px] left-[80%] -top-[352px] bg-[rgba(255,89,89,0.6)] mix-blend-screen opacity-[0.16] blur-[22.5px] rotate-[30.25deg]" />
       <div className="absolute  hidden lg:block lg:w-[100px] lg:h-[1388px] left-[56%] -top-[287px] bg-[rgba(255,89,89,0.6)] mix-blend-screen opacity-[0.16] blur-[22.5px] rotate-[40.65deg]" />
     </div>
--- src/app/challenges/[challengeId]/page.tsx
+++ src/app/challenges/[challengeId]/page.tsx
@@ -23,124 +23,59 @@ export default async function ChallengeIntro({ params }: any) {
         <Timeline milestones={challenge.milestones} />
       </div>
 
-      <div className=" flex flex-wrap gap-16 mt-8">
-        <div className="w-full lg:flex-1 order-2 lg:order-1 flex flex-col gap-8">
-          <div className="max-w-full">
-            <h3 className="body-3 font-bold mb-8">Official Links</h3>
-            <div className="w-full my-4">
-              {challenge.socialLinks.twitterURI ? (
-                <div className="flex border-b border-b-grey-800 py-4 gap-3">
-                  <Image
-                    src="/icons/twitter.svg"
-                    height="20"
-                    width="20"
-                    alt="twitter"
-                  ></Image>
-                  <p className="body-3"> {challenge.socialLinks.twitterURI} </p>
-                </div>
-              ) : null}
-              {challenge.socialLinks.discordURI ? (
-                <div className="flex border-b border-b-grey-800 py-4 gap-3">
-                  <Image
-                    src="/icons/discord.svg"
-                    height="20"
-                    width="20"
-                    alt="discord"
-                  ></Image>
-                  <p className="body-3"> {challenge.socialLinks.discordURI} </p>
-                </div>
-              ) : null}
+      <div className="flex gap-4 items-center">
+        <div className="heading-3">TL;DR</div>
+        <p className="body-2 text-white">{challenge.description}</p>
+      </div>
+      <div className="my-10">
+        {/* <h3 className="body-3 font-bold mb-8">Prize Rewards</h3> */}
 
-              {challenge.socialLinks.officialWebsiteURI ? (
-                <div className="flex border-b border-b-grey-800 py-4 gap-3">
-                  <Image
-                    src="/icons/globe.svg"
-                    height="20"
-                    width="20"
-                    alt="twitter"
-                  ></Image>
-                  <p className="body-3">
-                    {challenge.socialLinks.officialWebsiteURI}
-                  </p>
+        <div className="border border-grey-700 p-4">
+          {awards.map((award, index) => (
+            <div
+              key={index}
+              className={clsx('flex p-3  mb-2 ', {
+                'border-b border-b-grey-700': index !== awards.length - 1,
+              })}
+            >
+              <div className="flex-1">
+                <p className="body-3 py-2">{award.awardName}</p>
+                <div className="flex justify-between body-4 text-grey-400">
+                  <p>{formatMoney(award.amount)}</p>
+                  <p>x {award.count}</p>
                 </div>
-              ) : null}
-              {challenge.socialLinks.email ? (
-                <div className="flex border-b border-b-grey-800 py-4 gap-3">
-                  <Image
-                    src="/icons/email.svg"
-                    height="20"
-                    width="20"
-                    alt="twitter"
-                  ></Image>
-                  <p className="body-3"> {challenge.socialLinks.email} </p>
-                </div>
-              ) : null}
+              </div>
             </div>
-          </div>
-          <div className="max-w-full">
-            <h3 className="body-3 font-bold mb-8">Prize Rewards</h3>
-
-            <div className="border border-grey-700 p-4">
-              {awards.map((award, index) => (
-                <div
-                  key={index}
-                  className={clsx('flex p-3  mb-2 ', {
-                    'border-b border-b-grey-700': index !== awards.length - 1,
-                  })}
-                >
-                  <div className="flex-1">
-                    <p className="body-3 py-2">{award.awardName}</p>
-                    <div className="flex justify-between body-4 text-grey-400">
-                      <p>{formatMoney(award.amount)}</p>
-                      <p>x {award.count}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="max-w-full">
-            <h3 className="body-3 font-bold mb-8">Organizer & Sponsors</h3>
-            <div className="grid gap-4 grid-cols-2">
-              {sponsorships.map((sponsorship, index) => (
-                <div
-                  key={sponsorship.id}
-                  className={clsx('border border-grey-400', {
-                    'col-span-2': index === 0,
-                  })}
-                >
-                  <Aspect ratio={3}>
-                    <Image
-                      width={450}
-                      height={150}
-                      src={unsplash('sponsor')}
-                      alt="crypto"
-                      className="object-cover w-full h-full"
-                    />
-                  </Aspect>
-                  <div
-                    className={clsx('flex justify-center items-end h-6', {
-                      'bg-gradient-to-r from-[#F62584] to-[#480CA7]':
-                        index === 0,
-                      'bg-grey-400': index !== 0,
-                    })}
-                  >
-                    <div className="body-3">
-                      {formatMoney(sponsorship.amount)}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+          ))}
         </div>
-
-        <div className="w-full lg:flex-[3] order-1 lg:order-2">
-          <h3 className="body-3 font-bold mb-8">Description</h3>
-          <p className="body-3 text-grey-400">{challenge.description}</p>
-          <hr className="my-4 border-grey-800" />
-          <h3 className="body-3 font-bold my-8">Requirements</h3>
+      </div>
+      <div>
+        <h3 className="heading-4 font-bold my-8 text-center">
+          Speaker & Judges
+        </h3>
+        <div className="flex flex-row flex-wrap gap-8">
+          {challenge.judges.map((judge) => {
+            return (
+              <div key={judge.id} className="w-[150px]">
+                <Aspect ratio={1 / 1}>
+                  <Image
+                    height={150}
+                    width={150}
+                    src={unsplash('women')}
+                    className="object-cover w-full h-full"
+                    alt={judge.name}
+                  />
+                </Aspect>
+                <p className="body-3 mt-6">{judge.name}</p>
+                <p className="body-4 text-grey-400">{judge.organization}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-full flex gap-8 flex-col md:flex-row my-16">
+        <div className="flex-1 p-8 bg-white/5 border border-grey-800">
+          <h3 className="heading-5 font-bold mb-8">Requirements & Rules</h3>
           <ul>
             {challenge.requirements.map((r) => (
               <li key={r} className="list-disc list-inside text-grey-400">
@@ -148,31 +83,106 @@ export default async function ChallengeIntro({ params }: any) {
               </li>
             ))}
           </ul>
-          <hr className="my-4 border-grey-800" />
+        </div>
+        <div className="flex-1 p-8 bg-white/5 border border-grey-800">
+          <h3 className="heading-5 font-bold mb-8">Judging Criteria</h3>
+          <ul>
+            {challenge.requirements.map((r) => (
+              <li key={r} className="list-disc list-inside text-grey-400">
+                <span className="body-3 text-grey-400">{r}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
 
-          <h3 className="body-3 font-bold my-8">Location</h3>
-          <p className="body-3 text-grey-400">{challenge.location}</p>
-          <hr className="my-4 border-grey-800" />
+      <div className="w-full lg:flex-1 order-2 lg:order-1 flex flex-col gap-8">
+        <div className="max-w-full">
+          <h3 className="heading-4 font-bold my-8 text-center">
+            Official Links
+          </h3>
+          <div className="w-full my-4">
+            {challenge.socialLinks.twitterURI ? (
+              <div className="flex border-b border-b-grey-800 py-4 gap-3">
+                <Image
+                  src="/icons/twitter.svg"
+                  height="20"
+                  width="20"
+                  alt="twitter"
+                ></Image>
+                <p className="body-3"> {challenge.socialLinks.twitterURI} </p>
+              </div>
+            ) : null}
+            {challenge.socialLinks.discordURI ? (
+              <div className="flex border-b border-b-grey-800 py-4 gap-3">
+                <Image
+                  src="/icons/discord.svg"
+                  height="20"
+                  width="20"
+                  alt="discord"
+                ></Image>
+                <p className="body-3"> {challenge.socialLinks.discordURI} </p>
+              </div>
+            ) : null}
 
-          <h3 className="body-3 font-bold my-8">Speaker & Judges</h3>
-          <div className="flex flex-row flex-wrap gap-8">
-            {challenge.judges.map((judge) => {
-              return (
-                <div key={judge.id} className="w-[150px]">
-                  <Aspect ratio={1 / 1}>
-                    <Image
-                      height={150}
-                      width={150}
-                      src={unsplash('women')}
-                      className="object-cover w-full h-full"
-                      alt={judge.name}
-                    />
-                  </Aspect>
-                  <p className="body-3 mt-6">{judge.name}</p>
-                  <p className="body-4 text-grey-400">{judge.organization}</p>
+            {challenge.socialLinks.officialWebsiteURI ? (
+              <div className="flex border-b border-b-grey-800 py-4 gap-3">
+                <Image
+                  src="/icons/globe.svg"
+                  height="20"
+                  width="20"
+                  alt="twitter"
+                ></Image>
+                <p className="body-3">
+                  {challenge.socialLinks.officialWebsiteURI}
+                </p>
+              </div>
+            ) : null}
+            {challenge.socialLinks.email ? (
+              <div className="flex border-b border-b-grey-800 py-4 gap-3">
+                <Image
+                  src="/icons/email.svg"
+                  height="20"
+                  width="20"
+                  alt="twitter"
+                ></Image>
+                <p className="body-3"> {challenge.socialLinks.email} </p>
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+        <div className="max-w-full">
+          <h3 className="body-3 font-bold mb-8">Organizer & Sponsors</h3>
+          <div className="grid gap-4 grid-cols-2">
+            {sponsorships.map((sponsorship, index) => (
+              <div
+                key={sponsorship.id}
+                className={clsx('border border-grey-400', {
+                  'col-span-2': index === 0,
+                })}
+              >
+                <Aspect ratio={3}>
+                  <Image
+                    width={450}
+                    height={150}
+                    src={unsplash('sponsor')}
+                    alt="crypto"
+                    className="object-cover w-full h-full"
+                  />
+                </Aspect>
+                <div
+                  className={clsx('flex justify-center items-end h-6', {
+                    'bg-gradient-to-r from-[#F62584] to-[#480CA7]': index === 0,
+                    'bg-grey-400': index !== 0,
+                  })}
+                >
+                  <div className="body-3">
+                    {formatMoney(sponsorship.amount)}
+                  </div>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </div>
       </div>
