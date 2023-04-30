864bb54033be8d4cecafc361cfeca924d0c7f373
Nicolas Jiang <nicolazj@gmail.com>
2023-05-01 10:10:58 +1200

    update redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default async function Home() {
   let challenges = await getChallenges();
   let active = challenges.filter((c) => c.status === 'ACTIVE');
   if (active.length === 1) {
-    return redirect(`/challenges/${active[0].id}`);
+    // return redirect(`/challenges/${active[0].id}`);
+    return redirect(`/challenges/bewater-web3-cn`);
   }
   return redirect('/challenges');
 }
