251f8ebece0fa7fe58b7c68c018b2e557c195110
muvich3n <muvich3n@gmail.com>
2023-04-30 15:25:40 +0800

    intro updated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -24,9 +24,9 @@ export const HeaderImpl = ({ logo, nav, user }: HeaderImplProps) => {
         <div className="w-1/2 order-1 md:w-1/5 flex justify-start h-[72px] items-center">
           {logo}
         </div>
-        <div className="w-full order-3 md:order-2 md:w-3/5 flex justify-center ">
+        {/* <div className="w-full order-3 md:order-2 md:w-3/5 hidden md:flex justify-center ">
           {nav}
-        </div>
+        </div> */}
         <div className="w-1/2  order-2 md:order-3 md:w-1/5 flex justify-end h-[72px] items-center">
           {user}
         </div>
@@ -38,7 +38,7 @@ export const HeaderImpl = ({ logo, nav, user }: HeaderImplProps) => {
 
 const BeWaterLogo = () => {
   return (
-    <Link href="/" className="block relative">
+    <Link href="/" className="flex flex-row relative">
       <Image
         src="/logo/bewater-h.svg"
         width={120}
@@ -48,6 +48,12 @@ const BeWaterLogo = () => {
       <div className="body-5 text-day absolute left-full top-[-12px] rounded-full p-[1px] bg-gradient-108 from-[#057382] to-[#66FFFF]">
         <div className="bg-night rounded-full px-[5px] leading-[12px]">
           Alpha
+        </div>
+      </div>
+      <div className="absolute left-[164px] hidden sm:flex flex-row gap-2 items-center">
+        <div className="text-white body-2 font-bold">/</div>
+        <div className="text-day body-2 font-bold uppercase [text-shadow:0_0_6px_theme(colors.day)]">
+          Challenges
         </div>
       </div>
     </Link>
