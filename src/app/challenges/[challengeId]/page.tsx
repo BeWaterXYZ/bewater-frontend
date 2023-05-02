15ee00548b2a99c11bf7f7435ef6104efe52bfc3
muvich3n <muvich3n@gmail.com>
2023-05-02 11:29:00 +0800

    update bg & copy

--- src/app/challenges/[challengeId]/page.tsx
+++ src/app/challenges/[challengeId]/page.tsx
@@ -26,11 +26,11 @@ export default async function ChallengeIntro({ params }: any) {
     <div className="container flex flex-col gap-16 md:gap-30">
       <Timeline milestones={challenge.milestones} />
 
-      <div className="flex flex-col md:flex-row md:gap-20 items-center my-10">
+      <div className="flex flex-col gap-4 md:flex-row md:gap-20 items-center my-10">
         <div className="heading-5 md:heading-3 whitespace-nowrap py-4">
           赛事简介
         </div>
-        <div className="body-2 text-white">
+        <div className="body-3 md:body-2 text-white">
           {challenge.description.split('\n').map((s) => (
             <p className="py-3" key={s}>
               {s}
