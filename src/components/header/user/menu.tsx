51992a91bb6efbfd341e9d8999234c1625d5e0ee
Nicolas Jiang <nicolazj@gmail.com>
2023-05-03 15:05:50 +1200

    test menu

--- src/components/header/user/menu.tsx
+++ src/components/header/user/menu.tsx
@@ -21,59 +21,73 @@ export const UserMenu = () => {
   };
   if (error) return null;
   const user = data?.userProfile!;
+  if (!user) return null;
   return (
-    <NavigationMenu.Root className="relative">
-      <NavigationMenu.List className="list-none">
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="">
-            <Link href={isLoading ? '/onboarding' : '/settings/basic'}>
-              <Avatar
-                className="w-8 h-8 hover:opacity-75"
-                src={!isLoading ? user?.avatarURI : ''}
-                walletAddress={walletAddress}
-              />
-            </Link>
-          </NavigationMenu.Trigger>
-          {!isLoading && (
-            <NavigationMenu.Content className="absolute top-[100%] z-[11] rounded right-0 body-2 w-60 border border-midnight bg-night  shadow-midnight">
-              <div className="p-4">
-                <p className="body-2">
-                  {walletAddress?.substring(0, 8)}...
-                  {walletAddress?.substring(
-                    walletAddress.length - 8,
-                    walletAddress.length,
-                  )}
-                </p>
-              </div>
-              <ul className="font-medium ">
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link href={`/user/${user.userId}`} className="body-2">
-                    Your Profile
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link href="/settings/basic" className="body-2">
-                    Account Settings
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link
-                    href="/notifications/requests/received"
-                    className="body-2"
-                  >
-                    Notifications
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <button className="body-2" onClick={logout}>
-                    Disconnect
-                  </button>
-                </li>
-              </ul>
-            </NavigationMenu.Content>
-          )}
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
+    <div>
+      <Link href={`/user/${user.userId}`} className="body-2">
+        Your Profile
+      </Link>
+      <Link href="/settings/basic" className="body-2">
+        Account Settings
+      </Link>
+      <Link href="/notifications/requests/received" className="body-2">
+        Notifications
+      </Link>
+    </div>
   );
+  // return (
+  //   <NavigationMenu.Root className="relative">
+  //     <NavigationMenu.List className="list-none">
+  //       <NavigationMenu.Item>
+  //         <NavigationMenu.Trigger className="">
+  //           <Link href={isLoading ? '/onboarding' : '/settings/basic'}>
+  //             <Avatar
+  //               className="w-8 h-8 hover:opacity-75"
+  //               src={!isLoading ? user?.avatarURI : ''}
+  //               walletAddress={walletAddress}
+  //             />
+  //           </Link>
+  //         </NavigationMenu.Trigger>
+  //         {!isLoading && (
+  //           <NavigationMenu.Content className="absolute top-[100%] z-[11] rounded right-0 body-2 w-60 border border-midnight bg-night  shadow-midnight">
+  //             <div className="p-4">
+  //               <p className="body-2">
+  //                 {walletAddress?.substring(0, 8)}...
+  //                 {walletAddress?.substring(
+  //                   walletAddress.length - 8,
+  //                   walletAddress.length,
+  //                 )}
+  //               </p>
+  //             </div>
+  //             <ul className="font-medium ">
+  //               <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+  //                 <Link href={`/user/${user.userId}`} className="body-2">
+  //                   Your Profile
+  //                 </Link>
+  //               </li>
+  //               <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+  //                 <Link href="/settings/basic" className="body-2">
+  //                   Account Settings
+  //                 </Link>
+  //               </li>
+  //               <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+  //                 <Link
+  //                   href="/notifications/requests/received"
+  //                   className="body-2"
+  //                 >
+  //                   Notifications
+  //                 </Link>
+  //               </li>
+  //               <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+  //                 <button className="body-2" onClick={logout}>
+  //                   Disconnect
+  //                 </button>
+  //               </li>
+  //             </ul>
+  //           </NavigationMenu.Content>
+  //         )}
+  //       </NavigationMenu.Item>
+  //     </NavigationMenu.List>
+  //   </NavigationMenu.Root>
+  // );
 };
