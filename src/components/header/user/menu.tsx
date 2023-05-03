9fe9adcbad3935a488e0f2c7639c6befab541815
Nicolas Jiang <nicolazj@gmail.com>
2023-05-03 21:52:47 +1200

    test your profile

--- src/components/header/user/menu.tsx
+++ src/components/header/user/menu.tsx
@@ -14,75 +14,80 @@ export const UserMenu = () => {
     s?.walletAddress,
   ]);
 
-  const { error, data, isLoading } = useFetchUser(userId);
+  const { data, isLoading } = useFetchUser(userId);
   const logout = () => {
     clearStore();
     window.location.href = '/';
   };
-  if (error) return null;
+  if (!data) return null;
   const user = data?.userProfile!;
   return (
-    <NavigationMenu.Root className="relative">
-      <NavigationMenu.List className="list-none">
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="">
-            <Link href={isLoading ? '/onboarding' : '/settings/basic'}>
-              <Avatar
-                className="w-8 h-8 hover:opacity-75"
-                src={!isLoading ? user?.avatarURI : ''}
-                walletAddress={walletAddress}
-              />
-            </Link>
-          </NavigationMenu.Trigger>
-          {!isLoading && (
-            <NavigationMenu.Content className="absolute top-[100%] z-[11] rounded right-0 body-2 w-60 border border-midnight bg-night  shadow-midnight">
-              <div className="p-4">
-                <p className="body-2">
-                  {walletAddress?.substring(0, 8)}...
-                  {walletAddress?.substring(
-                    walletAddress.length - 8,
-                    walletAddress.length,
-                  )}
-                </p>
-              </div>
-              <ul className="font-medium ">
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link
-                    prefetch={false}
-                    href={`/user/${user.userId}`}
-                    className="body-2"
-                  >
-                    Your Profile
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link
-                    prefetch={false}
-                    href="/settings/basic"
-                    className="body-2"
-                  >
-                    Account Settings
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <Link
-                    prefetch={false}
-                    href="/notifications/requests/received"
-                    className="body-2"
-                  >
-                    Notifications
-                  </Link>
-                </li>
-                <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
-                  <button className="body-2" onClick={logout}>
-                    Disconnect
-                  </button>
-                </li>
-              </ul>
-            </NavigationMenu.Content>
-          )}
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
+    <>
+      <div className="opacity-0 absolute">
+        <Link href={`/user/${user.userId}`} className="body-2">
+          Your Profile
+        </Link>
+        <Link href="/settings/basic" className="body-2">
+          Account Settings
+        </Link>
+        <Link href="/notifications/requests/received" className="body-2">
+          Notifications
+        </Link>
+      </div>
+
+      <NavigationMenu.Root className="relative">
+        <NavigationMenu.List className="list-none">
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger className="">
+              <Link href={isLoading ? '/onboarding' : '/settings/basic'}>
+                <Avatar
+                  className="w-8 h-8 hover:opacity-75"
+                  src={!isLoading ? user?.avatarURI : ''}
+                  walletAddress={walletAddress}
+                />
+              </Link>
+            </NavigationMenu.Trigger>
+            {!isLoading && (
+              <NavigationMenu.Content className="absolute top-[100%] z-[11] rounded right-0 body-2 w-60 border border-midnight bg-night  shadow-midnight">
+                <div className="p-4">
+                  <p className="body-2">
+                    {walletAddress?.substring(0, 8)}...
+                    {walletAddress?.substring(
+                      walletAddress.length - 8,
+                      walletAddress.length,
+                    )}
+                  </p>
+                </div>
+                <ul className="font-medium ">
+                  <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+                    <Link href={`/user/${user.userId}`} className="body-2">
+                      Your Profile
+                    </Link>
+                  </li>
+                  <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+                    <Link href="/settings/basic" className="body-2">
+                      Account Settings
+                    </Link>
+                  </li>
+                  <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+                    <Link
+                      href="/notifications/requests/received"
+                      className="body-2"
+                    >
+                      Notifications
+                    </Link>
+                  </li>
+                  <li className="border-t p-4 py-2 border-midnight hover:bg-midnight transition-colors ease-out cursor-pointer">
+                    <button className="body-2" onClick={logout}>
+                      Disconnect
+                    </button>
+                  </li>
+                </ul>
+              </NavigationMenu.Content>
+            )}
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+    </>
   );
 };
