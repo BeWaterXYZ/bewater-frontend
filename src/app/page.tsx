56d2c48485eff9bb5eef625acfc916bbea7ec45c
Nicolas Jiang <nicolazj@gmail.com>
2023-06-02 14:39:03 +1200

    add clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
 export default async function Home() {
   return (
     <main className="">
-      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
