83d4bc23470a2e8f0535b4faa307f7fe846d47a6
Nicolas Jiang <nicolazj@gmail.com>
2023-06-05 11:40:35 +1200

    remove drizzle

--- drizzle/0000_sticky_mongoose.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "challenges" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"location" text NOT NULL,
-	"approved" boolean DEFAULT false NOT NULL,
-	"published" boolean DEFAULT false NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updatedAt" timestamp DEFAULT now() NOT NULL
-);
