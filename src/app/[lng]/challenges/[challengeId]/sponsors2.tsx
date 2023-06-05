e1e634b027d40cf36db4ce44dea5884c27fa4bb0
muvich3n <muvich3n@gmail.com>
2023-06-05 18:14:26 +0800

    Update Sponsors

--- src/app/[lng]/challenges/[challengeId]/sponsors2.tsx
+++ src/app/[lng]/challenges/[challengeId]/sponsors2.tsx
@@ -1,31 +1,18 @@
 const sponsors_tier1 = [
-  '/sponsors/ABCDE.png',
-  '/sponsors/aliyun.png',
-  '/sponsors/ScalingX.png',
+  '/sponsors/debox.png',
   '/sponsors/particleNetwork.png',
-  '/sponsors/Metatrust.png',
-  '/sponsors/seedao.png',
-  '/sponsors/cointime.png',
+  '/sponsors/bewater.png',
 ];
 
 const sponsors_tier2 = [
-  '/sponsors/hamster.png',
-  '/sponsors/BIXIN.png',
-  '/sponsors/ChainIDE.png',
-  '/sponsors/memoworld.png',
-  '/sponsors/bodl.png',
-  '/sponsors/filecoin.png',
-  '/sponsors/hkust.png',
-  '/sponsors/talentre.png',
-  '/sponsors/knn3.png',
-  '/sponsors/memolabs.png',
-  '/sponsors/looksrare.png',
-  '/sponsors/debox.png',
+  '/sponsors/vip3.png',
+  '/sponsors/sinohope.png',
+  'sponsors/club3.png',
 ];
+
 const sponsors_tier3 = [
   '/sponsors/sf.png',
   '/sponsors/csdn.png',
-  '/sponsors/noncegeek.png',
   '/sponsors/moledao.png',
   '/sponsors/abga.png',
   '/sponsors/thuba.png',
