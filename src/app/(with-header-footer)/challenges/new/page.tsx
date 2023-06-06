9e454ca0eb3aa09d7fe3cfb98d2e0f13651e8a3b
Nicolas Jiang <nicolazj@gmail.com>
2023-06-07 11:45:56 +1200

    wip

--- src/app/(with-header-footer)/challenges/new/page.tsx
+++ src/app/(with-header-footer)/challenges/new/page.tsx
@@ -1,32 +1,46 @@
 "use client";
 import { Input } from "@/components/form/input";
+import { Radio } from "@/components/form/radio";
 import { TextArea } from "@/components/form/textarea";
 import { validationSchema } from "@/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const schema = z
-  .object({
-    title: validationSchema.text,
-    hostName: validationSchema.text,
-    description: validationSchema.text,
-  })
-  .required();
-
-export type Inputs = z.infer<typeof schema>;
-
 export default function Page() {
+  const [isOnlineOnly, isOnlineOnlySet] = useState(true);
+  const schema = z
+    .object({
+      title: validationSchema.text,
+      hostName: validationSchema.text,
+      description: validationSchema.text,
+      location: validationSchema.text,
+      city: z.string().optional(),
+    })
+    .required();
+
+  type Inputs = z.infer<typeof schema>;
+
   let {
     control,
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
+    watch,
   } = useForm<Inputs>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      location: "online",
+    },
   });
-
-  let onSubmit = (formData: Inputs) => {};
+  let onSubmit = (formData: Inputs) => {
+    console.log({ formData });
+  };
+  watch((data) => {
+    isOnlineOnlySet(data.location === "online");
+  });
 
   return (
     <div className="container my-4 pt-20 flex flex-1 ">
@@ -58,6 +72,30 @@ export default function Page() {
               error={errors["description"]}
             />
           </fieldset>
+          <fieldset className="py-2">
+            <label className="block text-[12px] py-1 text-grey-500 font-bold  ">
+              Campaign mode
+            </label>
+            <Radio
+              control={control}
+              name="location"
+              onValueChange={(v) => setValue("location", v)}
+              options={[
+                { value: "online", label: "Online" },
+                { value: "offline", label: "Offline" },
+                { value: "mixed", label: "Online + Offline" },
+              ]}
+            />
+          </fieldset>
+          {isOnlineOnly ? null : (
+            <fieldset className="py-2">
+              <Input
+                label="City"
+                {...register("city")}
+                error={errors["city"]}
+              />
+            </fieldset>
+          )}
 
           <div className="py-8 flex justify-end">
             <button className="btn btn-primary" type="submit">
@@ -69,4 +107,3 @@ export default function Page() {
     </div>
   );
 }
-``;
---
+++ src/components/form/radio.tsx
@@ -0,0 +1,60 @@
+import * as RadioGroup from "@radix-ui/react-radio-group";
+import clsx from "clsx";
+import React, { useId } from "react";
+import { Controller } from "react-hook-form";
+
+interface RadioProps extends React.ComponentPropsWithoutRef<"input"> {
+  options: {
+    value: string;
+    label: React.ReactNode;
+  }[];
+  onValueChange: (v: string) => void;
+  control: any;
+}
+
+export const Radio = React.forwardRef(function Radio_(
+  { options, name, onValueChange, control }: RadioProps,
+  ref: any
+) {
+  let id = useId();
+
+  return (
+    <Controller
+      name={name!}
+      control={control}
+      render={({ field }) => {
+        return (
+          <RadioGroup.Root
+            className="flex flex-wrap gap-4"
+            defaultValue={field.value?.toString()}
+            value={field.value?.toString()}
+            onValueChange={onValueChange}
+          >
+            {options.map((op) => {
+              return (
+                <div
+                  className={clsx(
+                    "flex-1 flex gap-2 items-center border border-white/10  p-4",
+                    field.value === op.value ? "bg-white/10" : "bg-white/[0.02]"
+                  )}
+                  key={id + op.value}
+                >
+                  <RadioGroup.Item
+                    className="bg-white h-6 w-6 rounded-full "
+                    value={op.value}
+                    id={id + op.value + "-item"}
+                  >
+                    <RadioGroup.Indicator className=" flex items-center justify-center relative w-full h-full rounded-full bg-day after:content-[''] after:block after:w-[12px] after:h-[12px] after:rounded-full after:bg-white" />
+                  </RadioGroup.Item>
+                  <label className="" htmlFor={id + op.value + "-item"}>
+                    {op.label}
+                  </label>
+                </div>
+              );
+            })}
+          </RadioGroup.Root>
+        );
+      }}
+    ></Controller>
+  );
+});
