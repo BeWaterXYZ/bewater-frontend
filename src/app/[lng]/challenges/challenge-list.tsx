d34fba3d6bfe584d899bb2d6dfa8f7e4ab692df5
muvich3n <muvich3n@gmail.com>
2023-06-07 18:59:29 +0800

    Replace Challenge List Cover

--- src/app/[lng]/challenges/challenge-list.tsx
+++ src/app/[lng]/challenges/challenge-list.tsx
@@ -22,7 +22,7 @@ export function ChallengeList({ challenges, lng }: ChallengeListProps) {
             <Aspect ratio={5 / 2}>
               <Image
                 fill
-                src={`/challenge/assets/${challenge.id}.png`}
+                src={`/challenge/assets/${challenge.id}withTitle.png`}
                 alt="crypto"
                 className="object-cover w-full h-full"
               />
