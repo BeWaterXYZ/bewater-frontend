2676314c3f749086621e80b7f25da02de8a2bb7f
muvich3n <muvich3n@gmail.com>
2023-06-09 18:27:29 +0800

    Update track section

--- src/app/[lng]/challenges/[challengeId]/prize-section/63c82bd12ddc570f32ada869.tsx
+++ src/app/[lng]/challenges/[challengeId]/prize-section/63c82bd12ddc570f32ada869.tsx
@@ -1,31 +1,126 @@
 import PrizeList from '@/components/prize-list';
 import SponsorsCell from '@/components/sponsor-marquee-cell';
 import Marquee from 'react-fast-marquee';
+import Image from 'next/image';
 
 export function PrizeSection() {
   return (
     <div className="flex flex-col items-center py-20 px-0 gap-20 bg-[radial-gradient(210%_100%_at_50%_0%,_var(--tw-gradient-stops))] from-day/[0.15] via-night/0 to-day/[0.15] rounded-xl border-solid border-[1px] border-midnight">
       <h3 className="heading-5 md:heading-3 text-day md:text-day [text-shadow:0_4px_36px_rgba(0_255_255_/_0.4)] text-center">
-        赛道设置
+        赛事奖励：vDBX + USDT
       </h3>
 
       <div className="flex flex-col items-center gap-16">
         <div className="flex flex-col items-center gap-10">
-          {/* <p className="body-1 md:heading-5 font-bold text-white/30 md:text-white/30">
-            主赛道 A - 应用类
-          </p> */}
-          <div className="flex flex-row gap-[min(32px,2vw)] ">
-            <PrizeList title="🎭 表情包" />
-            <PrizeList title="👤 PFP" />
+          <p className="body-1 md:heading-5 font-bold text-white/30 md:text-white/30">
+            赛道设置
+          </p>
+          <div className="flex flex-col gap-4 md:gap-7 items-center">
+            <p className="body-3 md:body-1 uppercase text-[#00cccc] md:text-[#00cccc]">
+              🎭 表情包
+            </p>
+            <div className="prizeList2 w-auto max-w-[620px] mx-5 px-5 py-5">
+              <div className="flex z-[1] flex-col sm:flex-row items-center gap-5 w-full">
+                <Image
+                  src={`/challenge/assets/emojis.png`}
+                  alt="emoji"
+                  width={160}
+                  height={160}
+                  className="w-full sm:w-40 sm:h-40"
+                />
+                <ul className="body-3 text-white/60 list-disc list-inside">
+                  <li>
+                    展示你的幽默和创意，不限定主题，推荐主题包括
+                    “韭菜的自我修养”、“行走在熊市”、基于 DeBox 四款 NFT
+                    企鹅、小鹰、小蛇、小兔子相关的萌物生活、DeBox
+                    与影视作品相关段子，例如 DeBox 赌神、梭哈等。
+                  </li>
+                  <br />
+                  <li>
+                    我们建议参赛选手以组为单位制作表情包，一组约含4个表情包，越多越好。手工而非
+                    AI 制作的表情包将额外加分哦！
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center gap-10">
-          {/* <p className="body-1 md:heading-5 font-bold text-white/30 md:text-white/30">
-            主赛道 B - 基础设施类
-          </p> */}
-          <div className="flex flex-row gap-[min(32px,2vw)] ">
-            <PrizeList title="💻 ProgrammingGC" />
-            <PrizeList title="🎨 像素画" />
+          <div className="flex flex-col gap-4 md:gap-7 items-center">
+            <p className="body-3 md:body-1 uppercase text-[#00cccc] md:text-[#00cccc]">
+              👤 PFP
+            </p>
+            <div className="prizeList2 w-auto max-w-[620px] mx-5 px-5 py-5">
+              <div className="flex z-[1] flex-col sm:flex-row items-center gap-5 w-full">
+                <Image
+                  src={`/challenge/assets/pfp.png`}
+                  alt="emoji"
+                  width={160}
+                  height={160}
+                  className="w-full sm:w-40 sm:h-40"
+                />
+                <ul className="body-3 text-white/60 list-disc list-inside">
+                  <li>
+                    展示你的个性和风格，不限主题，推荐主题包括 “DeBox 四款 NFT
+                    的 PFP 重构”、“下一款蓝筹 PFP”、“社交类 PFP的形象”。手工而非
+                    AI 制作的 PFP 将获得额外加分。
+                  </li>
+                  <br />
+                  <li>
+                    设计师提交 PFP 时，需要提交至少 4 个同系列的
+                    PFP，并提供相应拆解的组件。同时，我们希望你能为每个PFP提供相关的世界观、故事以及创作过程中的插曲故事，通过文字故事与
+                    PFP 艺术结合，赋予 PFP 作品更多的内涵意义。
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 md:gap-7 items-center">
+            <p className="body-3 md:body-1 uppercase text-[#00cccc] md:text-[#00cccc]">
+              💻 PROGRAMMINGGC
+            </p>
+            <div className="prizeList2 w-auto max-w-[620px] mx-5 px-5 py-5">
+              <div className="flex z-[1] flex-col sm:flex-row items-center gap-5 w-full">
+                <Image
+                  src={`/challenge/assets/programminggc.png`}
+                  alt="emoji"
+                  width={160}
+                  height={160}
+                  className="w-full sm:w-40 sm:h-40"
+                />
+                <ul className="body-3 text-white/60 list-disc list-inside">
+                  <li>
+                    分形艺术的介绍请参考这里 https://zh.wikipedia.org/zh-tw/分形
+                  </li>
+                  <br />
+                  <li>
+                    必须借助程序生成，我们鼓励艺术家们和程序员进行合作，通过程序生成令人惊叹的艺术作品，探索分形艺术的魅力。
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 md:gap-7 items-center">
+            <p className="body-3 md:body-1 uppercase text-[#00cccc] md:text-[#00cccc]">
+              🎨 像素风{' '}
+            </p>
+            <div className="prizeList2 w-auto max-w-[620px] mx-5 px-5 py-5">
+              <div className="flex z-[1] flex-col sm:flex-row items-center gap-5 w-full">
+                <Image
+                  src={`/challenge/assets/pixel.png`}
+                  alt="emoji"
+                  width={160}
+                  height={160}
+                  className="w-full sm:w-40 sm:h-40"
+                />
+                <ul className="body-3 text-white/60 list-disc list-inside">
+                  <li>
+                    展示你对经典像素艺术的热爱，手工而非 AI
+                    制作的像素艺术作品将额外加分。
+                  </li>
+                  <br />
+                  <li>画布限定尺寸为 16x16、32x32、64x64、128x128。 </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
