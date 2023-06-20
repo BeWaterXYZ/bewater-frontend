3f4b341deec7862baa3214953030576df1d4e5e2
muvich3n <muvich3n@gmail.com>
2023-06-20 10:34:52 +0800

    Update SignIn & SIgnUp

--- src/app/(with-header-footer)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(with-header-footer)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,88 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
+import { dark } from '@clerk/themes';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center min-h-[100vh]">
-      <SignIn />
+    <div className="flex flex-col md:flex-row justify-center items-center min-h-[100vh] gap-10 md:gap-20 px-20">
+      <div>
+        <div className="flex flex-col gap-10 w-[440px]">
+          <p className="text-xl font-bold text-day">
+            Want to host something wonderful on BeWater?
+          </p>
+          <Link 
+          className='group relative flex'
+          href="/sign-up">
+            {/* 2 row 2 col grid */}
+            <div className="grid grid-cols-2 grid-rows-2 gap-3">
+              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+                <Image
+                  src="/assets/hackathon.png"
+                  width={64}
+                  height={64}
+                  alt="hackathon"
+                />
+                <p className="text-white/50 text-base font-bold">Hackathon</p>
+              </div>
+              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+                <Image
+                  src="/assets/demoday.png"
+                  width={64}
+                  height={64}
+                  alt="demoday"
+                />
+                <p className="text-white/50 text-base font-bold">Demoday</p>
+              </div>
+              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+                <Image
+                  src="/assets/workshop.png"
+                  width={64}
+                  height={64}
+                  alt="workshop"
+                />
+                <p className="text-white/50 text-base font-bold">Workshop</p>
+              </div>
+              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+                <Image
+                  src="/assets/other.png"
+                  width={64}
+                  height={64}
+                  alt="other"
+                />
+                <p className="text-white/50 text-base font-bold">Others</p>
+              </div>
+            </div>
+            <div className='absolute flex opacity-0 items-center justify-center z-10 w-[412px] h-full rounded-[4px] bg-white/5 text-4xl backdrop-blur-sm group-hover:opacity-100 transition-opacity ease-out duration-300 text-day [text-shadow:0_0_6px_theme(colors.day)] uppercase'>Sign up now</div>
+          </Link>
+        </div>
+      </div>
+      {/* responsive divider, vertical when wide screenm horizontal when small screen */}
+      <div className="w-full h-px md:w-px md:h-[276px] bg-gray-800"></div>
+
+      <SignIn
+        appearance={{
+          baseTheme: dark,
+          variables: { colorPrimary: '#00ffff' },
+          elements: {
+            formButtonPrimary:
+              'bg-day text-night hover:bg-[#00cccc] active:bg-[#009999] rounded-sm focus:shadow-none',
+            card: 'bg-night text-white p-0 gap-10',
+            headerSubtitle: 'text-gray-500',
+            socialButtons: 'hidden',
+            dividerRow: 'hidden',
+            formFieldInput:
+              'bg-night text-white border-gray-800 rounded-sm placeholder-gray-600',
+            formFieldLabel: 'text-gray-500 hidden',
+            formFieldLabelRow: 'mb-2',
+            footer: 'hidden',
+            header: 'text-xl gap-2',
+            identityPreviewEditButton: 'text-gray-500',
+            formResendCodeLink:
+              'text-day hover:text-[#00cccc] active:text-[#009999] rounded-none focus:shadow-none',
+          },
+        }}
+      />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,42 @@ const jetBrainsMono = JetBrains_Mono({
   weight: "variable",
 });
 
+// https://github.com/clerkinc/javascript/blob/main/packages/localizations/src/en-US.ts
+
+const localization = {
+  formFieldInputPlaceholder__emailAddress: 'Enter your email',
+  signIn: {
+    start: {
+      title: 'Already have a host account?',
+      subtitle: 'Sign in to manage your campaigns',
+      actionText: '',
+      actionLink: '',
+    },
+    emailCode: {
+      title: 'Verify your email',
+      subtitle: '',
+      formTitle: '',
+      formSubtitle: 'Enter the verification code sent to your email address',
+      resendButton: 'Resend code',
+    },
+  },
+  signUp: {
+    start: {
+      title: 'Create a host account',
+      subtitle: 'Sign up to create your campaigns',
+      actionText: 'Already have a host account?',
+      actionLink: 'Sign in',
+    },
+    emailCode: {
+      title: 'Verify your email',
+      subtitle: '',
+      formTitle: '',
+      formSubtitle: 'Enter the verification code sent to your email address',
+      resendButton: 'Resend code',
+    },
+  },
+}
+
 export const metadata = {
   title: "BeWater Host website",
   description: "BeWater Host website",
@@ -21,7 +57,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={localization}>
       <QueryProvider>
         <html lang="en">
           <body className={jetBrainsMono.className}>
