390e1ff63e2e9df75144ed93009fda93bc0d10bf
Nicolas Jiang <nicolazj@gmail.com>
2023-06-28 13:29:44 +1200

    Update page.tsx

--- src/app/[lng]/campaigns/page.tsx
+++ src/app/[lng]/campaigns/page.tsx
@@ -46,7 +46,7 @@ export default async function ChallengePage({
       {/* todo , fixme  */}
       {completed.length > 0 && (
         <div className="pt-4 pb-8">
-          <h2 className="heading-5">Completed</h2>
+          <h2 className="body-1 text-[24px] uppercase">Completed</h2>
           <ChallengeList challenges={completed} lng={lng} />
         </div>
       )}
