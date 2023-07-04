d347ef27b6fc268170ca229461b4f949522865a6
Nicolas Jiang <nicolazj@gmail.com>
2023-07-04 22:17:50 +1200

    update

--- src/app/(with-header-footer)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(with-header-footer)/sign-in/[[...sign-in]]/page.tsx
@@ -1,22 +1,20 @@
-import { SignIn } from '@clerk/nextjs';
-import Image from 'next/image';
-import { dark } from '@clerk/themes';
-import Link from 'next/link';
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+import { dark } from "@clerk/themes";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center min-h-[100vh] gap-10 md:gap-20 px-20">
+    <div className="container my-4 pt-20 flex flex-col lg:flex-row justify-center items-center min-h-[100vh] gap-10 md:gap-20 ">
       <div>
-        <div className="flex flex-col gap-10 w-[440px]">
+        <div className="flex flex-col gap-10 ">
           <p className="text-xl font-bold text-day">
             Want to host something wonderful on BeWater?
           </p>
-          <Link 
-          className='group relative flex'
-          href="/sign-up">
+          <Link className="group relative flex" href="/sign-up">
             {/* 2 row 2 col grid */}
-            <div className="grid grid-cols-2 grid-rows-2 gap-3">
-              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 grid-rows-2 gap-3">
+              <div className="flex items-center gap-3  py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
                 <Image
                   src="/assets/hackathon.png"
                   width={64}
@@ -25,7 +23,7 @@ export default function Page() {
                 />
                 <p className="text-white/50 text-base font-bold">Hackathon</p>
               </div>
-              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+              <div className="flex items-center gap-3 py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
                 <Image
                   src="/assets/demoday.png"
                   width={64}
@@ -34,7 +32,7 @@ export default function Page() {
                 />
                 <p className="text-white/50 text-base font-bold">Demoday</p>
               </div>
-              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+              <div className="flex items-center gap-3  py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
                 <Image
                   src="/assets/workshop.png"
                   width={64}
@@ -43,7 +41,7 @@ export default function Page() {
                 />
                 <p className="text-white/50 text-base font-bold">Workshop</p>
               </div>
-              <div className="flex items-center gap-3 w-[200px] py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
+              <div className="flex items-center gap-3  py-4 bg-white/[0.05] border-[0.5px] border-white/10 rounded-[4px] justify-center">
                 <Image
                   src="/assets/other.png"
                   width={64}
@@ -53,32 +51,34 @@ export default function Page() {
                 <p className="text-white/50 text-base font-bold">Others</p>
               </div>
             </div>
-            <div className='absolute flex opacity-0 items-center justify-center z-10 w-[412px] h-full rounded-[4px] bg-white/5 text-4xl backdrop-blur-sm group-hover:opacity-100 transition-opacity ease-out duration-300 text-day [text-shadow:0_0_6px_theme(colors.day)] uppercase'>Sign up now</div>
+            <div className="absolute flex opacity-0 items-center justify-center z-10 w-full h-full rounded-[4px] bg-white/5 text-4xl backdrop-blur-sm group-hover:opacity-100 transition-opacity ease-out duration-300 text-day [text-shadow:0_0_6px_theme(colors.day)] uppercase">
+              Sign up now
+            </div>
           </Link>
         </div>
       </div>
       {/* responsive divider, vertical when wide screenm horizontal when small screen */}
-      <div className="w-full h-px md:w-px md:h-[276px] bg-gray-800"></div>
+      <div className="w-full h-px lg:w-px lg:h-[276px] bg-gray-800"></div>
 
       <SignIn
         appearance={{
           baseTheme: dark,
-          variables: { colorPrimary: '#00ffff' },
+          variables: { colorPrimary: "#00ffff" },
           elements: {
             formButtonPrimary:
-              'bg-day text-night hover:bg-[#00cccc] active:bg-[#009999] rounded-sm focus:shadow-none',
-            card: 'bg-night text-white p-0 gap-10',
-            headerSubtitle: 'text-gray-500',
-            socialButtons: 'hidden',
-            dividerRow: 'hidden',
+              "bg-day text-night hover:bg-[#00cccc] active:bg-[#009999] rounded-sm focus:shadow-none",
+            card: "bg-night text-white p-0 gap-10",
+            headerSubtitle: "text-gray-500",
+            socialButtons: "hidden",
+            dividerRow: "hidden",
             formFieldInput:
-              'bg-night text-white border-gray-800 rounded-sm placeholder-gray-600',
-            formFieldLabel: 'text-gray-500 hidden',
-            formFieldLabelRow: 'mb-2',
-            header: 'text-xl gap-2',
-            identityPreviewEditButton: 'text-gray-500',
+              "bg-night text-white border-gray-800 rounded-sm placeholder-gray-600",
+            formFieldLabel: "text-gray-500 hidden",
+            formFieldLabelRow: "mb-2",
+            header: "text-xl gap-2",
+            identityPreviewEditButton: "text-gray-500",
             formResendCodeLink:
-              'text-day hover:text-[#00cccc] active:text-[#009999] rounded-none focus:shadow-none',
+              "text-day hover:text-[#00cccc] active:text-[#009999] rounded-none focus:shadow-none",
           },
         }}
       />
