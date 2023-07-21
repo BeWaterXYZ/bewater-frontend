44b6bf262d50f797d0f4886d7d3492a0c27aaf46
Fei Li <lifeign@proton.me>
2023-07-21 12:13:27 +0800

    event12 award

--- src/app/[lng]/campaigns/[challengeId]/page.tsx
+++ src/app/[lng]/campaigns/[challengeId]/page.tsx
@@ -274,9 +274,22 @@ export default async function ChallengeIntro({ params }: any) {
         ) : (
           <div className="container">
             <div className="flex flex-col items-center py-20 px-0 gap-20 bg-[radial-gradient(210%_100%_at_50%_0%,_var(--tw-gradient-stops))] from-day/[0.15] via-night/0 to-day/[0.15] rounded-xl border-solid border-[1px] border-midnight">
-              <h3 className="text-[24px] md:text-[36px] text-day md:text-day [text-shadow:0_4px_36px_rgba(0_255_255_/_0.4)] text-center">
-                Total Awards: ${challenge.totalAward} {challenge.awardCurrency}
-              </h3>
+              {challenge.yotadata?.award?.entitle ? (
+                <>
+                  <h3 className="text-[24px] md:text-[36px] text-day md:text-day [text-shadow:0_4px_36px_rgba(0_255_255_/_0.4)] text-center">
+                    {challenge.yotadata.award.entitle}
+                  </h3>
+                  <p className="body-3 md:body-1 text-day md:text-day [text-shadow:0_4px_36px_rgba(0_255_255_/_0.4)] text-center">
+                    Total Awards: ${challenge.totalAward}{' '}
+                    {challenge.awardCurrency}
+                  </p>
+                </>
+              ) : (
+                <h3 className="text-[24px] md:text-[36px] text-day md:text-day [text-shadow:0_4px_36px_rgba(0_255_255_/_0.4)] text-center">
+                  Total Awards: ${challenge.totalAward}{' '}
+                  {challenge.awardCurrency}
+                </h3>
+              )}
               <div className="flex flex-row flex-wrap items-top gap-16 p-8">
                 {(challenge.awardAssorts ?? []).map((awardAssort, i) => {
                   return (
@@ -331,6 +344,35 @@ export default async function ChallengeIntro({ params }: any) {
                   );
                 })}
               </div>
+              {challenge.yotadata?.award?.other ? (
+                <Link
+                  href={challenge.yotadata.award.other.link ?? ''}
+                  target="_blank"
+                >
+                  <div className="flex flex-row flex-wrap items-top gap-10 p-8 pt-0">
+                    {challenge.yotadata.award.other.entitle ? (
+                      <p className="body-3 text-white/60 text-center w-full">
+                        {challenge.yotadata.award.other.entitle}
+                      </p>
+                    ) : null}
+                    {challenge.yotadata.award.other.entext ? (
+                      <p className="body-4 text-white/60">
+                        {challenge.yotadata.award.other.entext}
+                      </p>
+                    ) : null}
+                    {challenge.yotadata.award.other.img ? (
+                      <Image
+                        src={challenge.yotadata.award.other.img}
+                        alt=""
+                        width={2242}
+                        height={882}
+                        className="w-full"
+                        style={{ borderRadius: '10px' }}
+                      />
+                    ) : null}
+                  </div>
+                </Link>
+              ) : null}
               {(challenge.keySponsors ?? []).length > 0 ? (
                 <div className="relative w-full flex flex-col gap-10 items-center">
                   <p className="body-1 md:text-[24px] font-bold text-white/30 md:text-white/30">
