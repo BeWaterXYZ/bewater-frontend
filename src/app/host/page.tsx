e9be09ff9d6b7a7f808c48daf6b506bcd95b774b
Nicolas Jiang <nicolazj@gmail.com>
2023-08-12 19:02:36 +1200

    update

--- src/app/host/page.tsx
+++ src/app/host/page.tsx
@@ -1,15 +1,14 @@
 import { Dashboard } from "./dashboard";
-import {SignedIn, SignedOut} from '@clerk/nextjs'
-import SignIn from './signup'
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import SignIn from "./signup";
 export default async function Home() {
   return (
     <div className="container my-4 pt-20 flex flex-1 ">
       <SignedIn>
-      <Dashboard />
-
+        <Dashboard />
       </SignedIn>
       <SignedOut>
-        <SignIn/>
+        <SignIn />
       </SignedOut>
     </div>
   );
