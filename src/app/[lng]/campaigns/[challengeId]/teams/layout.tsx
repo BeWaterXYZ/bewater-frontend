458f5ad14a35636a962be4d44bfa0a65a8395769
Fei Li <lifeign@proton.me>
2023-09-09 17:49:42 +0800

    chore:fix 显示的一个小问题

--- src/app/[lng]/campaigns/[challengeId]/teams/layout.tsx
+++ src/app/[lng]/campaigns/[challengeId]/teams/layout.tsx
@@ -2,6 +2,7 @@ import { CONFIGS } from '@/config';
 import { getChallengeById } from '@/services/challenge';
 import { Metadata } from 'next';
 import { segmentSchema } from '../param-schema';
+import { useTranslation } from '@/app/i18n';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return children;
@@ -9,9 +10,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const { challengeId } = segmentSchema.challengeId.parse(params);
+  const { lng } = segmentSchema.lng.parse(params);
   const challenge = await getChallengeById(challengeId);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { t } = await useTranslation(lng, 'translation');
+
   return {
-    title: 'BeWater - ' + challenge.title,
+    title: t('bewater') + ' - ' + challenge.title,
     description: challenge.description,
     twitter: {
       site: 'BeWater',
