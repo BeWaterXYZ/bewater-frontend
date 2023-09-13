ca477d0cc419a11622ad7cba276cbc4566ad5304
Nicolas Jiang <nicolazj@gmail.com>
2023-09-14 08:55:20 +1200

    Revert "fix:mac电脑footer屏幕适配"
    This reverts commit 3bdbffe2cc56c78fc5789cbce30e3ad3cff9f6fd.

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,41 +1,31 @@
 import { SignIn } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className="block ">
-      <div className=" container my-8 pt-24 flex flex-1 justify-center">
-        <SignIn
-          afterSignUpUrl={"/onboarding"}
-          appearance={{
-            baseTheme: dark,
-            variables: { colorPrimary: "#00ffff" },
-            elements: {
-              formButtonPrimary:
-                "bg-day text-night hover:bg-[#00cccc] active:bg-[#009999] rounded-sm focus:shadow-none",
-              card: "bg-night text-white p-0 gap-10",
-              headerSubtitle: "text-gray-500",
-              socialButtons: "",
-              dividerRow: "",
-              formFieldInput:
-                "bg-night text-white border-gray-800 rounded-sm placeholder-gray-600",
-              formFieldLabel: "text-gray-500 ",
-              formFieldLabelRow: "mb-2",
-              header: "text-xl gap-2",
-              identityPreviewEditButton: "text-gray-500",
-              formResendCodeLink:
-                "text-day hover:text-[#00cccc] active:text-[#009999] rounded-none focus:shadow-none",
-            },
-          }}
-        />
-      </div>
-      <Image
-        height={204}
-        width={468}
-        src={'/assets/space.png'}
-        className="block h-[300px]"
-        alt=""
+    <div className=" container my-4 pt-20 flex flex-1 justify-center">
+      <SignIn
+        afterSignUpUrl={"/onboarding"}
+        appearance={{
+          baseTheme: dark,
+          variables: { colorPrimary: "#00ffff" },
+          elements: {
+            formButtonPrimary:
+              "bg-day text-night hover:bg-[#00cccc] active:bg-[#009999] rounded-sm focus:shadow-none",
+            card: "bg-night text-white p-0 gap-10",
+            headerSubtitle: "text-gray-500",
+            socialButtons: "",
+            dividerRow: "",
+            formFieldInput:
+              "bg-night text-white border-gray-800 rounded-sm placeholder-gray-600",
+            formFieldLabel: "text-gray-500 ",
+            formFieldLabelRow: "mb-2",
+            header: "text-xl gap-2",
+            identityPreviewEditButton: "text-gray-500",
+            formResendCodeLink:
+              "text-day hover:text-[#00cccc] active:text-[#009999] rounded-none focus:shadow-none",
+          },
+        }}
       />
     </div>
   );
