fe449bd5e2ab2a7862a38bcbf217354bcc587e0f
Nicolas Jiang <nicolazj@gmail.com>
2023-09-29 14:34:29 +1300

    update

--- src/app/[lng]/campaigns/[challengeId]/agenda.tsx
+++ src/app/[lng]/campaigns/[challengeId]/agenda.tsx
@@ -72,6 +72,7 @@ export function Agenda() {
         <Tabs.List className="w-full flex" aria-label="agenda">
           {data.map((d, index) => (
             <Tabs.Trigger
+              key={index}
               className="border border-gray-800 flex-1 heading-6 py-5 data-[state=active]:bg-gray-900"
               value={"tab" + index}
             >
@@ -81,19 +82,25 @@ export function Agenda() {
         </Tabs.List>
         {data.map((d, index) => (
           <Tabs.Content
+            key={index}
             className="border border-gray-800 p-6"
             value={"tab" + index}
           >
             <p className="body-2">{d.time}</p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-              {d.locations.map((l) => {
+              {d.locations.map((l, index) => {
                 return (
-                  <div className="py-4">
+                  <div className="py-4" key={index}>
                     <p className="text-day">{l.location}</p>
-                    {l.sessions.map((s) => {
+                    {l.sessions.map((s, i) => {
                       return (
-                        <div className="flex flex-wrap items-center gap-4 py-3">
-                          <div className="p-3 w-40 text-center border border-gray-600 rounded-full">{s.time}</div>
+                        <div
+                          className="flex flex-wrap items-center gap-4 py-3"
+                          key={i}
+                        >
+                          <div className="p-3 w-40 text-center border border-gray-600 rounded-full">
+                            {s.time}
+                          </div>
                           <div>{s.topic}</div>
                         </div>
                       );
