3cce917eac77bd623131dc66b40d761bcfa2d2ca
michael wang <michael.w0932@gmail.com>
2023-10-20 00:11:28 +1300

    Update agenda and remove hard code.

--- src/app/[lng]/campaigns/[challengeId]/agenda.tsx
+++ src/app/[lng]/campaigns/[challengeId]/agenda.tsx
@@ -4,214 +4,18 @@ import { Challenge } from "@/services/types";
 import * as Tabs from "@radix-ui/react-tabs";
 import clsx from "clsx";
 
-let data = [
-  {
-    label: "Day1",
-    time: "22 OCT, GMT+8",
-    locations: [
-      {
-        location: "Room1",
-        sessions: [
-          { time: "Morning", topic: "Opening Ceremony & Talks" },
-          { time: "Afternoon", topic: "Workshops" },
-        ],
-      },
-      {
-        location: "Room2",
-        sessions: [
-          { time: "Morning", topic: "Joint Session with Room1" },
-          { time: "Afternoon", topic: "Panel & Talks" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Day2",
-    time: "23 OCT, GMT+8",
-    locations: [
-      {
-        location: "Room1",
-        sessions: [
-          { time: "Morning", topic: "Workshops" },
-          { time: "Afternoon", topic: "Workshops" },
-        ],
-      },
-      {
-        location: "Room2",
-        sessions: [
-          { time: "Morning", topic: "Panel & Talks" },
-          { time: "Afternoon", topic: "Panel & Talks" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Day3",
-    time: "24 OCT, GMT+8",
-    locations: [
-      {
-        location: "Room1",
-        sessions: [
-          { time: "Morning", topic: "Workshops" },
-          { time: "Afternoon", topic: "Workshops" },
-        ],
-      },
-      {
-        location: "Room2",
-        sessions: [
-          { time: "Morning", topic: "Demo Day" },
-          { time: "Afternoon", topic: "Demo Day" },
-        ],
-      },
-    ],
-  },
-];
-let data61 = [
-  {
-    label: "Fri, Oct 13",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "12:00 AM", topic: "Check-in starts" },
-          { time: "06:00 PM", topic: "Dinner" },
-          {
-            time: "07:00 PM",
-            topic: "Project introduction (2 minutes for each team)",
-          },
-          { time: "12:00 PM", topic: "Check-in ends" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Sat, Oct 14 - Sun, Oct 15",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "All Day", topic: "Project Buidl" },
-          { time: "12:00 AM", topic: "Lunch" },
-          { time: "01:00 PM", topic: "Workshop" },
-          { time: "06:00 PM", topic: "Dinner" },
-          { time: "07:00 PM", topic: "Project progress report" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Mon, Oct 16",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "All Day", topic: "Project Buidl" },
-          { time: "12:00 AM", topic: "Lunch" },
-          {
-            time: "02:00 PM",
-            topic: "Project submission & First round of judging",
-          },
-          { time: "06:00 PM", topic: "Dinner" },
-          {
-            time: "07:00 PM",
-            topic:
-              "Demo Day (5 minutes for each project in the top 5 of each track)",
-          },
-          { time: "09:00 PM", topic: "After Party" },
-        ],
-      },
-    ],
-  },
-];
-
-let data71 = [
-  {
-    label: "10月14日",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "13:30", topic: "签到" },
-          { time: "14:00", topic: "主持人开场" },
-          { time: "14:05", topic: "主办方致辞: Starbase" },
-          { time: "14:25", topic: "在Telegram中做去中心化小程序" },
-          { time: "14:40", topic: "Q&A" },
-          { time: "14:50", topic: "圆桌" },
-          { time: "15:40", topic: "5 * 8握手会: 初创企业Keynote" },
-          { time: "16:50", topic: "自由交流（茶歇）" },
-        ],
-      },
-    ],
-  },
-];
-
-
-
-let data62 = [
-  {
-    label: "Day 1: OCT 22nd",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "14:00pm-14:30pm", topic: "Scroll & BeWater Opening Speech" },
-          { time: "14:30pm-15:00pm", topic: "Web 3.0 标准化探索 " },
-          { time: "15:30pm-16:00pm", topic: "Decentralised Social On Top Of Centralised Platforms" },
-          { time: "16:00pm-16:30pm", topic: "Builder-First AI Security" },
-          { time: "16:30pm-17:00pm", topic: "Explore zk with modularity on Ethereum" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Day 2: OCT 23nd",
-    time: "",
-    locations: [
-      {
-        location: "",
-        sessions: [
-          { time: "10:00-10:45", topic: "Deploy Zero Knowledge Proofs with Aleo" },
-          { time: "11:00-11:45", topic: "Intro to Scroll cross chain interactions" },
-          { time: "12:00-12:45", topic: "The grand finale of intentional development-AIBot collaborative generative blockchain development" },
-          { time: "13:00-14:00", topic: "ZK Proofs and the Polygon zkEVM    " },
-          { time: "14:30-15:15", topic: "Private Key Management - AWS Nitro Enclave Deep Dive and Demo" },
-          { time: "16:30-17:15", topic: "Why exactly do engineers fail to learn ZK? " },
-          { time: "15:30-16:15", topic: "Building with Chainlink CCIP" },
-        ],
-      },
-    ],
-  },
-];
-function getData(challenge: Challenge) {
-  // console.log(challenge);
-  if (challenge.id === "61") {
-    return data61;
-  }
-  if (challenge.id === "62") {
-    return data62;
-  }
-  if (challenge.id === "71") {
-    return data71;
-  }
-  return null;
-}
 interface Props {
   challenge: Challenge;
 }
 export function Agenda(props: Props) {
-  const data = getData(props.challenge);
-  if (!data) return null;
+  const { agenda } = props.challenge;
+  if (!agenda || agenda.length == 0) return null;
   return (
     <div className="text-white body-1">
       <p className="heading-4 text-center py-12">Agenda</p>
       <Tabs.Root className="w-full" defaultValue="tab0">
         <Tabs.List className="w-full flex" aria-label="agenda">
-          {data.map((d, index) => (
+          {agenda.map((d, index) => (
             <Tabs.Trigger
               key={index}
               className="border border-gray-800 flex-1 heading-6 py-5 data-[state=active]:bg-gray-900"
@@ -221,7 +25,7 @@ export function Agenda(props: Props) {
             </Tabs.Trigger>
           ))}
         </Tabs.List>
-        {data.map((d, index) => (
+        {agenda.map((d, index) => (
           <Tabs.Content
             key={index}
             className="border border-gray-800 p-6"
@@ -230,24 +34,39 @@ export function Agenda(props: Props) {
             <p className="body-2">{d.time}</p>
             <div
               className={clsx(
-                "grid grid-cols-1  gap-4 ",
+                "grid grid-cols-1 gap-4 text-base ",
                 d.locations.length > 1 ? "md:grid-cols-2" : "md:grid-cols-1"
               )}
             >
               {d.locations.map((l, index) => {
                 return (
                   <div className="py-4" key={index}>
-                    <p className="text-day">{l.location}</p>
+                    <p className="text-day font-bold my-2">{l.location}</p>
                     {l.sessions.map((s, i) => {
                       return (
                         <div
-                          className="flex flex-wrap items-center gap-4 py-3"
+                          className="flex flex-wrap items-center gap-4 py-3 font-normal"
                           key={i}
                         >
-                          <div className="p-3 w-40 text-center border border-gray-600 rounded-full">
+                          <div className="p-3 w-40 text-center border border-gray-600 rounded-full font-normal">
                             {s.time}
                           </div>
-                          <div>{s.topic}</div>
+                          <div className="p-3 w-80">
+                            {s.topic}
+                            {s.speaker && 
+                            <div className="text-xs pt-1 flex flex-row flex-1 opacity-60">
+                              {s.speaker.name && 
+                                <div className="pr-4">
+                                  {s.speaker.name}
+                                </div>
+                              }
+                              {s.speaker.title && 
+                                <div>
+                                  {s.speaker.title}
+                                </div>
+                              }
+                            </div>}  
+                          </div>
                         </div>
                       );
                     })}
