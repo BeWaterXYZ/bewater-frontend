9f14ce0c3844a0332e0d70e702ed73ae9b13b7aa
Fei Li <lifeign@proton.me>
2023-10-24 13:24:43 +0800

    feat:shortlist UI逻辑实现

--- src/app/host/challenges/[challengeId]/shortlist/page.tsx
+++ src/app/host/challenges/[challengeId]/shortlist/page.tsx
@@ -1,29 +1,11 @@
-import getSymbolFromCurrency from "currency-symbol-map";
-import { formatMoney } from "@/utils/numeral";
-import { Aspect } from "@/components/aspect";
-import HoverCard from "@/components/hover-card";
-import { useLoadingStoreAction } from "@/components/loading/store";
-import { getShortlistByChallenge } from "@/services/challenge";
-import { getChallengeTProjects } from "@/services/project"
-import { useFetchChallengeById } from "@/services/challenge.query";
-import { Judge, Project } from "@/services/types";
-import { formatYYYYMMMDD } from "@/utils/date";
-import {
-  CheckIcon,
-  OpenInNewWindowIcon,
-  TwitterLogoIcon,
-} from "@radix-ui/react-icons";
-import Image from "next/image";
-import Link from "next/link";
-import { Fragment, useEffect, useState } from "react";
-import Marquee from "react-fast-marquee";
-import Balancer from "react-wrap-balancer";
-import Markdown from "@/components/markdown";
 import { segmentSchema } from "../../../segment-params";
-import async from "react-select/dist/declarations/src/async/index";
+import { getChallengeTProjects } from "@/services/project"
+import { getShortlistByChallenge } from "@/services/challenge";
+import { Project } from "@/services/types";
+import { ProjectList } from './project-list';
 
 export default async function Page({ params }: any) {
-  let { challengeId } = segmentSchema.challengeId.parse(params);
+  const { challengeId } = segmentSchema.challengeId.parse(params);
   let projects: Project[] = await getChallengeTProjects(challengeId);
   const shortlist   = await getShortlistByChallenge(challengeId);
 
@@ -35,8 +17,6 @@ export default async function Page({ params }: any) {
   })
 
   return (
-    <div>
-      hello world
-    </div>
-  );
+    <ProjectList challengeId={challengeId} projects={projects} />
+  )
 }
