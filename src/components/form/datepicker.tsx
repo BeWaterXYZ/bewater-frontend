0ed0f07492a31adc460825b4d648b510be3c9be5
Nicolas Jiang <nicolazj@gmail.com>
2023-11-04 16:48:32 +1300

    update datepicker

--- src/components/form/datepicker.tsx
+++ src/components/form/datepicker.tsx
@@ -38,7 +38,7 @@ export const DatePicker = React.forwardRef(function DatePicker_(
           <div className={clsx("block group relative", className)}>
             {label ? (
               <label
-                className="absolute bottom-full left-0 text-xs text-grey-500  group-hover:text-day group-focus:text-day transition-colors"
+                className="block text-xs text-grey-500 font-bold mb-2 group-hover:text-day group-focus:text-day transition-colors"
                 htmlFor={id}
               >
                 {label}
@@ -49,6 +49,7 @@ export const DatePicker = React.forwardRef(function DatePicker_(
             <div className=" relative w-full text-[14px] bg-night  block body-3   rounded-sm text-white border border-midnight hover:!border-day focus:!border-day focus:outline-none transition-colors">
               <ReactDatePicker
                 disabled={disabled}
+                wrapperClassName="w-full"
                 className="w-full bg-white/0 h-10 px-2 outline-none"
                 placeholderText={placeholder}
                 selected={field.value ? parseISO(field.value as string) : null}
