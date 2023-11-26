ad8c3b15c8797ddb15417a3775cdffece7f8bf74
Nicolas Jiang <nicolazj@gmail.com>
2023-11-27 12:47:03 +1300

    fix host login

--- src/app/host/layout.tsx
+++ src/app/host/layout.tsx
@@ -1,19 +1,11 @@
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <SignedIn>
-        <div className=" ">
-          {children}
-          <Footer lng={"en"} />
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <SignIn />
-      </SignedOut>
-    </>
+    <div className=" ">
+      {children}
+      <Footer lng={"en"} />
+    </div>
   );
 }
