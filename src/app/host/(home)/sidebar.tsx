634819266d44392a8d6e039a36e13efbf2fcc560
sparus227 <sparus227@gmail.com>
2024-01-04 09:18:26 +0800

    feat: add upgrade popup

--- src/app/host/(home)/sidebar.tsx
+++ src/app/host/(home)/sidebar.tsx
@@ -118,6 +118,21 @@ export function Sidebar() {
           </Link>
         ))}
       </div>
+      <div className="fixed z-10 bg-[#25263C] bottom-0 w-[262px] mb-4 ml-2 py-[20px] px-4 rounded-lg">
+        <div className="absolute top-2 right-2">
+          <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M23 13L13 23M13 13L23 23" stroke="#98A2B3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+        <p className="font-semibold text-sm text-white mb-1">Used space</p>
+        <p className="text-sm text-[#F1F5F9] mb-4">Your team has used 80% of your available space. Need more?</p>
+        <div className="w-[230px] h-2 rounded bg-[#EAECF0]"></div>
+        <div className="w-[191.76px] h-2 rounded bg-[#00FFFF] relative top-[-8px] mb-2"></div>
+        <div>
+          <button className="font-semibold text-sm text-[#F1F5F9] mr-3">Dismiss</button>
+          <button className="font-semibold text-sm text-[#00FFFF]">Upgrade plan</button>
+        </div>
+      </div>
     </div>
   );
 }
