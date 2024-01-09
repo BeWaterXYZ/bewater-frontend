0a3b41a883497ea2ec786ce57d8bf24975ac1513
sparus227 <sparus227@gmail.com>
2024-01-09 22:24:22 +0800

    fix: host sidebar style

--- src/app/host/(home)/layout.tsx
+++ src/app/host/(home)/layout.tsx
@@ -1,16 +1,12 @@
-import { Header } from '@/components/header';
-import { Footer } from '@/components/footer';
-import { Sidebar } from './sidebar';
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { Sidebar } from "./sidebar";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-[calc(100vh-80px)] grid grid-cols-[200px,_1fr] gap-4">
+    <div className="h-[100vh] grid grid-cols-[200px,_1fr] gap-4">
       <Sidebar />
-      <div>{children}</div>
+      <div className="overflow-y-auto">{children}</div>
     </div>
   );
 }
