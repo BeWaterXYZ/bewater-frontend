ee71b3a103f52e900e59ccf56b89ac1e86dd27c9
sparus227 <sparus227@gmail.com>
2024-01-12 18:36:46 +0800

    chore: hide contestant

--- src/app/host/edit/[challengeId]/header.tsx
+++ src/app/host/edit/[challengeId]/header.tsx
@@ -24,24 +24,24 @@ const links = [
       </svg>
     ),
   },
-  {
-    path: "contestant",
-    label: "Contestant",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="none"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill="currentColor"
-          d="M11.5 8A1.5 1.5 0 0113 9.5v.5c0 1.971-1.86 4-5 4-3.14 0-5-2.029-5-4v-.5A1.5 1.5 0 014.5 8h7zm0 1h-7a.5.5 0 00-.5.5v.5c0 1.438 1.432 3 4 3s4-1.562 4-3v-.5a.5.5 0 00-.5-.5zM8 1.5A2.75 2.75 0 118 7a2.75 2.75 0 010-5.5zm0 1A1.75 1.75 0 108 6a1.75 1.75 0 000-3.5z"
-        ></path>
-      </svg>
-    ),
-  },
+  // {
+  //   path: "contestant",
+  //   label: "Contestant",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width="16"
+  //       height="16"
+  //       fill="none"
+  //       viewBox="0 0 16 16"
+  //     >
+  //       <path
+  //         fill="currentColor"
+  //         d="M11.5 8A1.5 1.5 0 0113 9.5v.5c0 1.971-1.86 4-5 4-3.14 0-5-2.029-5-4v-.5A1.5 1.5 0 014.5 8h7zm0 1h-7a.5.5 0 00-.5.5v.5c0 1.438 1.432 3 4 3s4-1.562 4-3v-.5a.5.5 0 00-.5-.5zM8 1.5A2.75 2.75 0 118 7a2.75 2.75 0 010-5.5zm0 1A1.75 1.75 0 108 6a1.75 1.75 0 000-3.5z"
+  //       ></path>
+  //     </svg>
+  //   ),
+  // },
   {
     path: "rating",
     label: "Rating",
